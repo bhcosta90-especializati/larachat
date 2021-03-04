69e660af0767acdc56aa16cd952a9917b5898f4d
Bruno Costa <bruno.costa@superlogica.com>
2021-03-04 12:06:09 -0300

    change seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create(['email' => 'first@local.com']);
+        \App\Models\User::factory(rand(20, 50))->create();
     }
 }
